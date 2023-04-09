2795c81eeb9b06a0386e1d7d3acb8723a937d886
itsmylifestyle <hahaii00@mail.ru>
2023-04-09 20:05:15 +0600

    Initial Commit

---
+++ musicplayer/musicplayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  musicplayerApp.swift
+//  musicplayer
+//
+//  Created by Айбек on 09.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct musicplayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
